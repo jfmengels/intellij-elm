f22f32a4a4b7558605c294716c4d6286a917846d
Jeroen Engels <jfm.engels@gmail.com>
2020-06-30 18:49:37 +0200

    Add a decoder for elm-review JSON output

---
+++ src/main/kotlin/org/elm/workspace/elmreview/ElmReviewJsonReport.kt
@@ -0,0 +1,90 @@
+package org.elm.workspace.elmreview
+
+
+import com.google.gson.Gson
+import org.elm.workspace.compiler.Chunk
+
+// The Elm compiler emits HTTP URLs with angle brackets around them
+private val urlPattern = Regex("""<((http|https)://.*?)>""")
+
+
+fun elmReviewJsonToMessages(json: String): List<ElmReviewError> {
+    val report = Gson().fromJson(json, Report::class.java) ?: error("failed to parse JSON report from elm-review")
+    return when (report) {
+        is Report.General -> {
+            listOf(
+//                    TODO Jeroen
+//                    ElmReviewError(
+//                    title = report.title,
+////                    html = chunksToHtml(report.message),
+//                    location = report.path?.let { ElmLocation(path = it, moduleName = null, region = null) })
+            )
+        }
+        is Report.Specific -> {
+            report.errors.flatMap { errorsForFile ->
+                errorsForFile.errors.map { error ->
+                    ElmReviewError(
+                            rule = error.rule,
+                            message = error.message,
+                            details = error.details,
+                            formatted = error.formatted,
+                            region = error.region,
+                            html = "<span>" + error.rule + "</span>"
+                    )
+                }
+            }
+        }
+    }
+}
+
+private fun chunksToHtml(chunks: List<Chunk>): String =
+        chunks.joinToString("",
+                prefix = "<html><body style=\"font-family: monospace; font-weight: bold\">",
+                postfix = "</body></html>"
+        ) { chunkToHtml(it) }
+
+private fun chunkToHtml(chunk: Chunk): String =
+        when (chunk) {
+            is Chunk.Unstyled -> toHtmlSpan("color: #4F9DA6", chunk.string)
+            is Chunk.Styled -> with(StringBuilder()) {
+                if (chunk.bold) append("font-weight: bold;")
+                if (chunk.underline) append("text-decoration: underline;")
+                append("color: ${chunk.color.adjustForDisplay()};")
+                toHtmlSpan(this, chunk.string)
+            }
+        }
+
+private fun toHtmlSpan(style: CharSequence, text: String) =
+        """<span style="$style">${text.convertWhitespaceToHtml().createHyperlinks()}</span>"""
+
+private fun String.createHyperlinks(): String =
+        urlPattern.replace(this) { result ->
+            val url = result.groupValues[1]
+            "<a href=\"$url\">$url</a>"
+        }
+
+/**
+ * The Elm compiler emits the text where the whitespace is already formatted to line up
+ * using a fixed-width font. But HTML does its own thing with whitespace. We could use a
+ * `<pre>` element, but then we wouldn't be able to do the color highlights and other
+ * formatting tricks.
+ *
+ * The best solution would be to use the "white-space: pre" CSS rule, but the UI widget
+ * that we use to render the HTML, [javax.swing.JTextPane], does not support it
+ * (as best I can tell).
+ *
+ * So we will instead manually convert the whitespace so that it renders correctly.
+ */
+private fun String.convertWhitespaceToHtml() =
+        replace(" ", "&nbsp;").replace("\n", "<br>")
+
+/**
+ * Adjust the Elm compiler's colors to make it look good
+ */
+private fun String?.adjustForDisplay(): String =
+        when (this) {
+            "yellow" -> "#FACF5A"
+            "red" -> "#FF5959"
+            null -> "white" // Elm compiler uses null to indicate default foreground color? who knows!
+            else -> this
+        }
---
+++ src/test/kotlin/org/elm/workspace/elmreview/ElmReviewJsonToHtmlTest.kt
@@ -0,0 +1,102 @@
+package org.elm.workspace.elmreview
+
+
+import junit.framework.TestCase
+import org.elm.lang.ElmTestBase
+import org.intellij.lang.annotations.Language
+
+
+class ElmReviewJsonToHtmlTest : ElmTestBase() {
+    fun `test specific error`() {
+        @Language("JSON")
+        val json = """
+            {
+              "type": "review-errors",
+              "errors": [
+                {
+                  "path": "src/NoUnused/Dependencies.elm",
+                  "errors": [
+                    {
+                      "message": "Top-level variable `fzef` is not used",
+                      "rule": "NoUnused.Variables",
+                      "details": [
+                        "You should either use this value somewhere, or remove it at the location I pointed at."
+                      ],
+                      "region": {
+                        "start": {
+                          "line": 48,
+                          "column": 1
+                        },
+                        "end": {
+                          "line": 48,
+                          "column": 5
+                        }
+                      },
+                      "fix": [
+                        {
+                          "range": {
+                            "start": {
+                              "line": 48,
+                              "column": 1
+                            },
+                            "end": {
+                              "line": 50,
+                              "column": 1
+                            }
+                          },
+                          "str": ""
+                        }
+                      ],
+                      "formatted": [
+                        {
+                          "str": "(fix) ",
+                          "color": [
+                            51,
+                            187,
+                            200
+                          ]
+                        },
+                        {
+                          "str": "NoUnused.Variables",
+                          "color": [
+                            255,
+                            0,
+                            0
+                          ]
+                        },
+                        ": Top-level variable `fzef` is not used\n\n48| fzef =\n    ",
+                        {
+                          "str": "^^^^",
+                          "color": [
+                            255,
+                            0,
+                            0
+                          ]
+                        },
+                        "\n49|     1\n\nYou should either use this value somewhere, or remove it at the location I pointed at."
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
+            """.trimIndent()
+
+        TestCase.assertEquals(elmReviewJsonToMessages(json),
+                listOf(ElmReviewError(
+                        rule = "NoUnused.Variables",
+                        message = "Top-level variable `fzef` is not used",
+                        details = listOf("You should either use this value somewhere, or remove it at the location I pointed at."),
+                        region = Region(Start(48, 1), End(48, 5)),
+                        formatted = listOf(
+                                Chunk.Styled(bold = false, underline = false, color = listOf(51, 187, 200), str = "(fix) "),
+                                Chunk.Styled(bold = false, underline = false, color = listOf(255, 0, 0), str = "NoUnused.Variables"),
+                                Chunk.Unstyled(str = ": Top-level variable `fzef` is not used\n\n48| fzef =\n    "),
+                                Chunk.Styled(bold = false, underline = false, color = listOf(255, 0, 0), str = "^^^^"),
+                                Chunk.Unstyled(str = "\n49|     1\n\nYou should either use this value somewhere, or remove it at the location I pointed at.")
+                        ),
+                        html = "<span>NoUnused.Variables</span>"
+                ))
+        )
+    }
+
